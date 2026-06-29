6a4bc8404907c39577c21af01801b001d7296eb2
Alexey Kuznetsov <popshow2011@gmail.com>
2026-06-30 02:52:23 +0400

    feat: add converter constants

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	const USD_TO_EUR = 0.88
+	const USD_TO_RUB = 82.50
+	const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
+	fmt.Println(EUR_TO_RUB)
+
+}
