8de59ea704ee2e78572dacc3aed120ae56632676
Alexey Kuznetsov <popshow2011@gmail.com>
2026-07-01 01:07:18 +0400

    feat: add converter func, userInput

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,27 +1,106 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const USD_TO_EUR = 0.88
 const USD_TO_RUB = 82.50
 const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
 func main() {
-	prompt, err := userPrompt()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	converter(prompt, "USD", "EUR")
+	initialCurr, value, targetCurr := getUserInput()
+	result := converter(value, initialCurr, targetCurr)
+	fmt.Println(result)
+
 }
 
-func converter(value int, initial, target string) {}
-
-func userPrompt() (int, error) {
-	var p int = 0
-	fmt.Scan(&p)
-
-	if p == 0 {
-		return 0, fmt.Errorf("[ERROR]: Пользователь ни чего не ввел")
+func availableCurrency(curr string) string {
+	switch strings.ToUpper(curr) {
+	case "USD":
+		return "EUR RUB"
+	case "EUR":
+		return "USD RUB"
+	default:
+		return "USD EUR"
 	}
-	return p, nil
+}
+
+func converter(value float64, initial, target string) float64 {
+	initial = strings.ToUpper(initial)
+	target = strings.ToUpper(target)
+
+	if initial == target {
+		return value
+	}
+
+	var inUSD float64
+	switch initial {
+	case "USD":
+		inUSD = value
+	case "EUR":
+		inUSD = value / USD_TO_EUR
+	case "RUB":
+		inUSD = value / USD_TO_RUB
+	}
+
+	switch target {
+	case "USD":
+		return inUSD
+	case "EUR":
+		return inUSD * USD_TO_EUR
+	case "RUB":
+		return inUSD * USD_TO_RUB
+	}
+
+	return 0
+}
+
+func getUserInput() (string, float64, string) {
+	var initialCurr string
+	var targetCurr string
+	var value float64
+
+	for initialCurr == "" {
+		fmt.Println("Введите наименование исходной валюты: USD RUB EUR")
+		_, err := fmt.Scan(&initialCurr)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		isInitial := strings.Contains("EUR RUB USD", strings.ToUpper(initialCurr))
+		if !isInitial {
+			fmt.Printf("Вы ввели недопустимую валюту - %s, попробуйте еще раз\n", initialCurr)
+			initialCurr = ""
+			continue
+		}
+	}
+
+	for value == 0 {
+		fmt.Println("Введите колличество валюты")
+		_, err := fmt.Scan(&value)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		if value <= 0 {
+			fmt.Println("Число должно быть больше 0")
+			value = 0
+			continue
+		}
+	}
+	for targetCurr == "" {
+		availableCurr := availableCurrency(initialCurr)
+		fmt.Printf("Введите наименование исходной валюты: %s \n", availableCurr)
+		_, err := fmt.Scan(&targetCurr)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		isTargetCurr := strings.Contains(strings.ToUpper(availableCurr), strings.ToUpper(targetCurr))
+		if !isTargetCurr {
+			fmt.Printf("Вы ввели недопустимую валюту - %s, попробуйте еще раз\n", availableCurr)
+			targetCurr = ""
+			continue
+		}
+	}
+	return initialCurr, value, targetCurr
 }
