3d874f1840bdb8e8cefbe8b461ec3388181b500a
Alexey Kuznetsov <popshow2011@gmail.com>
2026-06-30 22:51:41 +0400

    feat: add fun converter and prompt user

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	const USD_TO_EUR = 0.88
-	const USD_TO_RUB = 82.50
-	const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
-	fmt.Println(EUR_TO_RUB)
+const USD_TO_EUR = 0.88
+const USD_TO_RUB = 82.50
+const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
+func main() {
+	prompt, err := userPrompt()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	converter(prompt, "USD", "EUR")
+}
+
+func converter(value int, initial, target string) {}
+
+func userPrompt() (int, error) {
+	var p int = 0
+	fmt.Scan(&p)
+
+	if p == 0 {
+		return 0, fmt.Errorf("[ERROR]: Пользователь ни чего не ввел")
+	}
+	return p, nil
 }
