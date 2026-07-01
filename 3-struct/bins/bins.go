1284ef797848ef41e1f1176e2c6d83a76d95d24e
Alexey Kuznetsov <popshow2011@gmail.com>
2026-07-02 01:26:49 +0400

    feat: add structure project

---
+++ 3-struct/bins/bins.go
@@ -0,0 +1,30 @@
+package bins
+
+import "time"
+
+type Bin struct {
+	Id        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
+}
+
+type BinList struct {
+	Bin []Bin
+}
+
+func NewBin(id, name string, private bool) *Bin {
+	return &Bin{
+		Id:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+}
+func NewBinList(bins ...Bin) *BinList {
+
+	return &BinList{
+		Bin: bins,
+	}
+
+}
--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,34 +1,5 @@
 package main
 
-import "time"
-
-type Bin struct {
-	Id        string
-	Private   bool
-	CreatedAt time.Time
-	Name      string
-}
-
-type BinList struct {
-	Bin []Bin
-}
-
-func NewBin(id, name string, private bool) *Bin {
-	return &Bin{
-		Id:        id,
-		Private:   private,
-		CreatedAt: time.Now(),
-		Name:      name,
-	}
-}
-func NewBinList(bins ...Bin) *BinList {
-
-	return &BinList{
-		Bin: bins,
-	}
-
-}
-
 func main() {
 
 }
