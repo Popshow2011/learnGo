35ef76988eb60b0730a3509e6ac8ccf58fe0877e
Alexey Kuznetsov <popshow2011@gmail.com>
2026-07-03 12:51:57 +0400

    fix DI

--- 3-struct/api/api.go
+++ 3-struct/api/api.go
@@ -1,1 +1,33 @@
 package api
+
+import (
+	"encoding/json"
+	"test/3-struct/bins"
+	"test/3-struct/storage"
+)
+
+type API struct {
+	storage storage.Storage
+}
+
+func NewApi(s storage.Storage) *API {
+	return &API{
+		storage: s,
+	}
+}
+
+func (a *API) SaveBin(bin *bins.Bin) error {
+	data, _ := json.Marshal(bin)
+	_, err := a.storage.Write(data)
+	return err
+}
+
+func (a *API) LoadBins() (*bins.BinList, error) {
+	data, err := a.storage.Read()
+	if err != nil {
+		return nil, err
+	}
+	var binList bins.BinList
+	json.Unmarshal(data, &binList)
+	return &binList, nil
+}
--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,11 +1,22 @@
 package main
 
-import "test/3-struct/storage"
+import (
+	"fmt"
+	"test/3-struct/api"
+	"test/3-struct/bins"
+	"test/3-struct/storage"
+)
 
 const str = "asdasasd"
 
 func main() {
-	store := storage.NewStorage("data.json")
-	store.Write([]byte(str))
-	store.Read()
+	var store storage.Storage = storage.NewStorage("data.json")
+
+	apiService := api.NewApi(store)
+
+	bin := bins.NewBin("123", "MyBin", false)
+	apiService.SaveBin(bin)
+
+	loadedBins, _ := apiService.LoadBins()
+	fmt.Println(loadedBins)
 }
